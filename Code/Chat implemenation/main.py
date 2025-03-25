a428237855530d128f4b40625a837d9ab032eeb9
s221516 <s221516@student.dtu.dk>
2025-03-25 10:33:23 +0100

    heyme

---
+++ Code/Chat implemenation/main.py
@@ -0,0 +1,24 @@
+
+
+
+def transmit(message):
+    
+
+    
+
+
+
+
+
+def main():
+
+    while True:
+
+        message = input("Enter your message: ")
+
+        transmit(message)
+        response = receive()
+
+        #display(response)
+
+
