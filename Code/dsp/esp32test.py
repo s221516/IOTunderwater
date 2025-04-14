9552ecb96461fb0aad31b5aa10544960e6c7d2cc
Mathias Markvardsen <129381407+Markvardsen@users.noreply.github.com>
2025-04-14 16:16:43 +0200

    threading in the making

--- Code/dsp/esp32test.py
+++ Code/dsp/esp32test.py
@@ -3,8 +3,8 @@ import time
 
 # Set up the UART connection (adjust 'COM5' as needed)
 
-# ser = serial.Serial("/dev/cu.usbserial-0232D158", 115200, timeout=1)
-ser = serial.Serial("COM12", 115200, timeout=1)
+ser = serial.Serial("/dev/cu.usbserial-0232D158", 115200, timeout=1)
+# ser = serial.Serial("COM12", 115200, timeout=1)
 
 
 def read_line():
